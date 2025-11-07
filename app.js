cb21a3f4ad5bb9efcdc230152a131d8f972227c4
CatalogosHavard <catalogoshavard@gmail.com>
2025-11-07 14:11:33 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // --- ¡IMPORTANTE! ---
-// Pegá acá tu NUEVA URL que termina en output=tsv
+// Usá tu URL que termina en output=tsv
 const GOOGLE_SHEET_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTzsalmKmMs0A7Uo3JeO76oJVjWMotKYiFl3d4HuuLJ0oejUt_yO-plTgY4GiJdP8bo8-tGZXPlwtd4/pub?gid=0&single=true&output=tsv';
 // ---------------------
 
@@ -14,10 +14,7 @@ fetch(GOOGLE_SHEET_URL)
         for (let i = 1; i < filas.length; i++) {
             
             if (!filas[i]) continue; 
-
-            // --- ¡AQUÍ ESTÁ LA SOLUCIÓN! ---
-            // Cambiamos el separador de coma (,) a Tab (\t)
-            const celdas = filas[i].split('\t');
+            const celdas = filas[i].split('\t'); // Separador por Tab
 
             if (!celdas[0] || celdas[0] === "") {
                 continue;
@@ -27,13 +24,19 @@ fetch(GOOGLE_SHEET_URL)
             const sku = celdas[0];
             const producto = celdas[1];
             const precio = celdas[2];
-            // const imagenUrl = celdas[3];
+            
+            // --- ¡LÍNEA 1 DESCOMENTADA! ---
+            // Asumimos que la Columna 4 (índice 3) es la URL de la imagen
+            const imagenUrl = celdas[3];
 
             // Creamos la tarjeta del producto
             const productoCard = document.createElement('div');
             productoCard.className = 'producto-card'; 
 
             productoCard.innerHTML = `
+                
+                ${imagenUrl ? `<img src="${imagenUrl}" alt="${producto || ''}">` : ''}
+                
                 <h3>${producto || ''}</h3>
                 <p class="precio">$ ${precio || 'N/A'}</p>
                 <p class="sku">SKU: ${sku || ''}</p>
