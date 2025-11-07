32a32be9a7938abe6d3d85be16c710ec826495f1
CatalogosHavard <catalogoshavard@gmail.com>
2025-11-07 13:53:22 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // --- ¡IMPORTANTE! ---
 // Pegá acá la URL de tu Google Sheet publicado como .csv
-const GOOGLE_SHEET_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTzsalmKmMs0A7Uo3JeO76oJVjWMotKYiFl3d4HuuLJ0oejUt_yO-plTgY4GiJdP8bo8-tGZXPlwtd4/pub?output=csv';
+const GOOGLE_SHEET_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTzsalmKmMs0A7Uo3JeO76oJVjWMotKYiFl3d4HuuLJ0oejUt_yO-plTgY4GiJdP8bo8-tGZXPlwtd4/pub?gid=0&single=true&output=csv';
 // ---------------------
 
 // 1. Seleccionamos el contenedor que creamos en el HTML
@@ -51,4 +51,5 @@ fetch(GOOGLE_SHEET_URL)
         contenedor.innerHTML = '<p>Error al cargar productos. Intente más tarde.</p>';
 
     });
+
 
